90e740d591f0a912fb0822b39ea5ed389064c77b
ppang <yucaroll@naver.com>
2016-10-14 01:09:29 +0900

    menu 추가, kakaoLogin 추가.

---
+++ TravelMate/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  TravelMate
+//
+//  Created by 김혜지 on 2016. 10. 14..
+//  Copyright © 2016년 이동규. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController{
+    
+    @IBAction func doKakaoLogin(_ sender: AnyObject) {
+        
+        let session: KOSession = KOSession.shared()
+        
+        if session.isOpen(){ // 이미 열린 세션이 있으면 닫는다.
+            session.close()
+        }
+        
+        session.presentingViewController = self.navigationController
+        session.open(completionHandler: { (error) -> Void in
+            session.presentingViewController = nil
+            
+            if !session.isOpen(){
+                print("Kakao Session Close")
+            }
+            }, authParams: nil, authTypes: [KOAuthType.talk.rawValue, KOAuthType.account.rawValue])
+    }
+    
+    @IBAction func doGuestLogin(_ sender: AnyObject) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        appDelegate.loadMainViewController()
+    }
+}
