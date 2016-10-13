3d3f8fe61eda20eec33bc05884d21cd0cb1b17e6
ppang <yucaroll@naver.com>
2016-10-13 21:07:53 +0900

    kakaosdk 추

--- TravelMate/TravelMate-Bridging-Header.h
+++ TravelMate/TravelMate-Bridging-Header.h
@@ -1,5 +1,6 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+#ifndef TravelMate_Bridging_Header_h
+#define TravelMate_Bridging_Header_h
 
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
 
+#endif
