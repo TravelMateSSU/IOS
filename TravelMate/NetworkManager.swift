15d567fb09d72648b0c28279fd6fa2097910987b
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-15 17:03:08 +0900

    NetworkManager Debug모드 없앰

--- TravelMate/NetworkManager.swift
+++ TravelMate/NetworkManager.swift
@@ -46,53 +46,33 @@ class NetworkManager {
     
     // 서버에서 코스 타임라인 리스트 load
     func loadCourseTimeline(time: Date, loadMoreCount: Int, _ completion : @escaping ([CourseModel]?, Int) -> Void) {
-        #if DEBUG
-            let tourManager = TourAPIManager()
-            tourManager.querySearchByKeyword(keyword: "서울", completion: {
-                spots in
-                let courses: [CourseModel] = []
-                var course: CourseModel!
-                for (index, spot) in spots.enumerated() {
-                    if index % 5 == 0 {
-                        let user = UserModel()
-                        user.id = "123456"
-                        user.name = "이동규"
-                        course = CourseModel(title: spot.title, content: "dfjlaksdjlfkjsaledkfjalksdjclksjfkladjsklfjsakldjfkladjsflkajsdlfkjaklsfjlkdjsalfkjsdlkfjaksldjflkasjdfklajskldfjaskldjfaklsjdfklasjdfkl", author: user, spots: [], createdAt: Int(Date().timeIntervalSince1970), status: .active)
-                        courses.append(course)
-                    }
-                    course.spots.append(spot)
+        let parameters: Parameters = ["time": time, "loadMoreCount": loadMoreCount]
+        Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
+            if response.result.isSuccess {
+                var courses: [CourseModel] = []
+                guard let JSON = response.result.value as? [String: AnyObject] else {
+                    print("No Json Data")
+                    completion(nil, -1)
+                    return
                 }
+                
+                guard let courseJSONs = JSON["courses"] as? [[String: AnyObject]] else {
+                    print("No courses JSON")
+                    completion(nil, -1)
+                    return
+                }
+                
+                for courseJSON in courseJSONs {
+                    let course = CourseModel(json: courseJSON)
+                    courses.append(course)
+                }
+                
                 completion(courses, 200)
-            })
-        #else
-            let parameters: Parameters = ["time": time, "loadMoreCount": loadMoreCount]
-            Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
-                if response.result.isSuccess {
-                    var courses: [CourseModel] = []
-                    guard let JSON = response.result.value as? [String: AnyObject] else {
-                        print("No Json Data")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    guard let courseJSONs = JSON["courses"] as? [[String: AnyObject]] else {
-                        print("No courses JSON")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    for courseJSON in courseJSONs {
-                        let course = CourseModel(json: courseJSON)
-                        courses.append(course)
-                    }
-                    
-                    completion(courses, 200)
-                } else {
-                    print("fail")
-                    completion(nil, 300/*result Code*/)
-                }
+            } else {
+                print("fail")
+                completion(nil, 300/*result Code*/)
             }
-        #endif
+        }
     }
     
     
@@ -135,125 +115,98 @@ class NetworkManager {
     
     // 서버에 코스 키워드 검색 요청
     func loadSpotsByKeyword(keyword: String, _ completion: @escaping ([SpotModel]?, Int) -> Void) {
-        #if DEBUG
-            let manager = TourAPIManager()
-            manager.querySearchByKeyword(keyword: searchText, completion: {
-                spots in
-                self.spots = spots
-                self.tableView.reloadData()
-            })
-        #else
-            let parameters: Parameters = ["keyword": keyword]
-            Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
-                if response.result.isSuccess {
-                    var spots: [SpotModel] = []
-                    guard let JSON = response.result.value as? [String: AnyObject] else {
-                        print("No Json Data")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    guard let spotJSONs = JSON["courses"] as? [[String: AnyObject]] else {
-                        print("No courses JSON")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    for spotJSON in spotJSONs {
-                        let spot = SpotModel()
-//                        spot.setDa
-                        spots.append(spot)
-                    }
-                    
-                    completion(spots, 200)
-                } else {
-                    print("fail")
-                    completion(nil, 300/*result Code*/)
+        let parameters: Parameters = ["keyword": keyword]
+        Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
+            if response.result.isSuccess {
+                var spots: [SpotModel] = []
+                guard let JSON = response.result.value as? [String: AnyObject] else {
+                    print("No Json Data")
+                    completion(nil, -1)
+                    return
                 }
+                
+                guard let spotJSONs = JSON["courses"] as? [[String: AnyObject]] else {
+                    print("No courses JSON")
+                    completion(nil, -1)
+                    return
+                }
+                
+                for spotJSON in spotJSONs {
+                    let spot = SpotModel()
+                    //                        spot.setDa
+                    spots.append(spot)
+                }
+                
+                completion(spots, 200)
+            } else {
+                print("fail")
+                completion(nil, 300/*result Code*/)
             }
-        #endif
+        }
     }
     
     
     // 서버에 코스 키워드 검색 요청
     func loadCourseByKeyword(keyword: String, _ completion: @escaping ([CourseModel]?, Int) -> Void) {
-        #if DEBUG
-            let manager = TourAPIManager()
-            manager.querySearchByKeyword(keyword: searchText, completion: {
-                spots in
-                self.spots = spots
-                self.tableView.reloadData()
-            })
-        #else
-            let parameters: Parameters = ["keyword": keyword]
-            Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
-                if response.result.isSuccess {
-                    var courses: [CourseModel] = []
-                    guard let JSON = response.result.value as? [String: AnyObject] else {
-                        print("No Json Data")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    guard let courseJSONs = JSON["courses"] as? [[String: AnyObject]] else {
-                        print("No courses JSON")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    for courseJSON in courseJSONs {
-                        let course = CourseModel(json: courseJSON)
-                        courses.append(course)
-                    }
-                    
-                    completion(courses, 200)
-                } else {
-                    print("fail")
-                    completion(nil, 300/*result Code*/)
+        let parameters: Parameters = ["keyword": keyword]
+        Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
+            if response.result.isSuccess {
+                var courses: [CourseModel] = []
+                guard let JSON = response.result.value as? [String: AnyObject] else {
+                    print("No Json Data")
+                    completion(nil, -1)
+                    return
                 }
+                
+                guard let courseJSONs = JSON["courses"] as? [[String: AnyObject]] else {
+                    print("No courses JSON")
+                    completion(nil, -1)
+                    return
+                }
+                
+                for courseJSON in courseJSONs {
+                    let course = CourseModel(json: courseJSON)
+                    courses.append(course)
+                }
+                
+                completion(courses, 200)
+            } else {
+                print("fail")
+                completion(nil, 300/*result Code*/)
             }
-        #endif
+        }
     }
     
     
     // 서버에 검색 Spot을 포함하는 Course 요청
     func loadCourseBySpot(spotId: String, _ completion: @escaping ([CourseModel]?, Int) -> Void) {
-        #if DEBUG
-            let manager = TourAPIManager()
-            manager.querySearchByKeyword(keyword: searchText, completion: {
-                spots in
-                self.spots = spots
-                self.tableView.reloadData()
-            })
-        #else
-            let parameters: Parameters = ["spotId": spotId]
-            Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
-                if response.result.isSuccess {
-                    var courses: [CourseModel] = []
-                    guard let JSON = response.result.value as? [String: AnyObject] else {
-                        print("No Json Data")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    guard let courseJSONs = JSON["courses"] as? [[String: AnyObject]] else {
-                        print("No courses JSON")
-                        completion(nil, -1)
-                        return
-                    }
-                    
-                    for courseJSON in courseJSONs {
-                        let course = CourseModel(json: courseJSON)
-                        courses.append(course)
-                    }
-                    
-                    completion(courses, 200)
-                } else {
-                    print("fail")
-                    completion(nil, 300/*result Code*/)
+        let parameters: Parameters = ["spotId": spotId]
+        Alamofire.request(BASE_URL, method: .post, parameters: parameters).responseJSON { response in
+            if response.result.isSuccess {
+                var courses: [CourseModel] = []
+                guard let JSON = response.result.value as? [String: AnyObject] else {
+                    print("No Json Data")
+                    completion(nil, -1)
+                    return
                 }
+                
+                guard let courseJSONs = JSON["courses"] as? [[String: AnyObject]] else {
+                    print("No courses JSON")
+                    completion(nil, -1)
+                    return
+                }
+                
+                for courseJSON in courseJSONs {
+                    let course = CourseModel(json: courseJSON)
+                    courses.append(course)
+                }
+                
+                completion(courses, 200)
+            } else {
+                print("fail")
+                completion(nil, 300/*result Code*/)
             }
-        #endif
+        }
     }
     
     
